77d8ebb73427b501a94e3363386d113105374e35
Georg Stach <info@GeorgStach.de>
2025-02-15 00:42:49 +0100

    include CustomerServiceImpl again. Annotation problem fixed?

--- src/main/java/com/example/grpc/CustomerServiceImpl.java
+++ src/main/java/com/example/grpc/CustomerServiceImpl.java
@@ -1,19 +1,21 @@
 package com.example.grpc;
 
+import customer.CustomerRequest;
+import customer.CustomerResponse;
+import customer.CustomerServiceGrpc;
+import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 
 @GrpcService
-public class CustomerServiceImpl
-//        extends CustomerServiceGrpc.CustomerServiceImplBase
-{
+public class CustomerServiceImpl extends CustomerServiceGrpc.CustomerServiceImplBase  {
 
-//    @Override
-//    public void getCustomer(Customer.CustomerRequest request, StreamObserver<Customer.CustomerResponse> responseObserver) {
-//        Customer.CustomerResponse response = Customer.CustomerResponse.newBuilder()
-//                .setId(request.getId())
-//                .setName("John Doe")
-//                .build();
-//        responseObserver.onNext(response);
-//        responseObserver.onCompleted();
-//    }
+    @Override
+    public void getCustomer(CustomerRequest request, StreamObserver<CustomerResponse> responseObserver) {
+        CustomerResponse response = CustomerResponse.newBuilder()
+                .setId(request.getId())
+                .setName("John Doe")
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
 }
